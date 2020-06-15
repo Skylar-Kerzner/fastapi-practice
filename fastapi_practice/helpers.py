9e5a2ea9b3da780bb314d7689c4f6ad565ca012d
Skylar Kerzner <skylar.kerzner@gmail.com>
2020-06-15 12:47:27 -0700

    Correctly writes to all db fields

--- fastapi_practice/helpers.py
+++ fastapi_practice/helpers.py
@@ -7,7 +7,7 @@ def create_wikistats_entry(common_words_counts, page_name):
     word_1_count = max(common_words_counts.values())
 
     for i, (word, count) in enumerate(common_words_counts.items()):
-        wikistats_entry.word_ranked_1 = word
-        wikistats_entry.relative_frequency_1 = round(float(count)/word_1_count, 4)
+        exec('wikistats_entry.word_ranked_'+str(i+1)+' = word')
+        exec('wikistats_entry.relative_frequency_'+str(i+1)+' = round(float(count)/word_1_count, 4)')
  
     return wikistats_entry
