6be184f81cd0cf2f7c49faa34bd3f698fd337c10
Skylar Kerzner <skylar.kerzner@gmail.com>
2020-06-15 12:56:01 -0700

    added DELETE http method to delete databse

--- fastapi_practice/main.py
+++ fastapi_practice/main.py
@@ -10,10 +10,15 @@ from helpers import create_wikistats_entry
 import logging
 
 app = FastAPI()
+"""
+1. Given a search term, pull text from closest wikipedia page
+2. Calculate word stats that show the zipf principle
+3. Write stats to db
+4. Retrieve stats from db
+"""
 
 models.Base.metadata.create_all(bind=engine)
-# Background task in same container as API for this version. 
-# Then use dramatiq to put worker in separate container in another version
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -27,7 +32,9 @@ def get_db():
     finally:
         db.close()
 
-# Write stats to the db that show the zipf principle
+
+# Background task in same container as API for this version. 
+# Then use dramatiq to put worker in separate container in another version
 def write_stats_to_db(search_term, db):
     common_words_counts, tags_counts, page_name, total_words = get_most_common_words_tags_counts(search_term)
     logger.debug(f"{common_words_counts}")
@@ -38,30 +45,29 @@ def write_stats_to_db(search_term, db):
     logger.info(f"Committed {page_name} entry to db")
     return
 
+# POST: Run task and write results to db
 @app.post("/wikicounts/")
-async def read_item(search: Search, background_tasks : BackgroundTasks, db: Session = Depends(get_db)):
+async def add_item(search: Search, background_tasks : BackgroundTasks, db: Session = Depends(get_db)):
     background_tasks.add_task(write_stats_to_db, search.term, db)
     return {"message" : f"Running analytics on wikipage closest to '{search.term}'"}
 
 
 
-# The get
+# GET: Retrieve data from db
 @app.get("/wikicounts/")
-async def read_item(db: Session = Depends(get_db)):
+async def read_items(db: Session = Depends(get_db)):
     results = db.query(Wikistats).all()
     logger.debug(f"{results}")
     return {"results" : results}
 
 
-# Trivial practice endpoints
-@app.get("/")
-def read_root():
-    return {"Hello": {
-                "one" : "World",
-                "two" : "worlds"
-                    }
-            }
+# : Delete db
+@app.delete("/wikicounts/delete/")
+async def delete_items(db: Session = Depends(get_db)):
+    db.query(Wikistats).delete()
+    db.commit()
+    results = db.query(Wikistats).all()
+    logger.warning(f"DELETED DATABASE")
+    return {"results" : results}
 
-@app.get("/inputs/{item_id}")
-def read_item(item_id: str, q: List[str] = Query(None)): # List[str] = Query(None)
-    return {"item_id": item_id, "q": q}
+
